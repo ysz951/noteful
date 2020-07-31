af5d94d4727739a9d7e02e4dcac353afc589a434
ysz951 <shengyang_zhou@baylor.edu>
2020-07-30 23:27:06 -0500

    change config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://arcane-beach-66600.herokuapp.com',
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT,
     API_KEY: process.env.REACT_APP_API_KEY,
-    // API_ENDPOINT: 'http://localhost:8000'
-    // API_ENDPOINT: 'http://localhost:9090'
 }
